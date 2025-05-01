83223d7bf2573875ccecfc19540fb5461ecd59f2
Florisvdaa <129622961+Florisvdaa@users.noreply.github.com>
2025-05-01 08:26:14 +0200

    asset chahnges

---
+++ Assets/Scripts/Particle/PixelExplosionSpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelExplosionSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject pixelCubePrefab;
+    [SerializeField] private int minCubes = 3;
+    [SerializeField] private int maxCubes = 10;
+    [SerializeField] private float explosionForce = 5f;
+    [SerializeField] private float explosionRadius = 2f;
+    [SerializeField] private float upwardModifier = 1f;
+
+    public void Explode()
+    {
+        int cubeCount = Random.Range(minCubes, maxCubes + 1);
+
+        for (int i = 0; i < cubeCount; i++)
+        {
+            GameObject cube = Instantiate(pixelCubePrefab, transform.position, Random.rotation);
+            Rigidbody rb = cube.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                Vector3 explosionPos = transform.position;
+                rb.AddExplosionForce(explosionForce, explosionPos, explosionRadius, upwardModifier, ForceMode.Impulse);
+            }
+
+            Destroy(cube, 3f); // auto-clean after 3 seconds
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Explode();
+        }
+    }
+}
