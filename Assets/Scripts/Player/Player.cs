017c4ba18783cf1f4971aa81c003defd41cec59a
Florisvdaa <129622961+Florisvdaa@users.noreply.github.com>
2025-12-14 14:31:28 +0100

    player movmenet

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,7 +4,61 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
-{ 
-    
+{
+    [Header("Player Movement settings")]
+    [SerializeField] private GameObject playerVisual;
+    private Rigidbody rb;
+
+    private PlayerInputActions inputActions;
+
+    private float movementSpeed;
+    private float dashSpeed;
+    private float dashDuation;
+
+    private PlayerSettings playerSettings;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        playerSettings = GetComponent<PlayerSettings>();
+        rb = GetComponent<Rigidbody>();
+        
+        if (playerSettings != null)
+        {
+            movementSpeed = playerSettings.DefaultMovementSpeed;
+            dashSpeed = playerSettings.DefaultDashSpeed;
+            dashDuation = playerSettings.DefaultDashDuration;
+        }
+    }
+
+    private void Start()
+    {
+        inputActions.player.Dash.performed += ctx => HandleDash();   
+    }
+
+    private void FixedUpdate()
+    {
+        TryMovement();
+    }
+
+    private void TryMovement()
+    {
+        Vector2 moveInput = inputActions.player.Move.ReadValue<Vector2>();
+        Vector3 move = new Vector3(moveInput.x, 0f, moveInput.y);
+        rb.MovePosition(rb.position + move * movementSpeed * Time.fixedDeltaTime);
+    }
+    private void HandleDash()
+    {
+        Debug.Log("Dash");
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();   
+    }
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
 }
 
--- Assets/Scripts/Player/PlayerSettings.cs
+++ Assets/Scripts/Player/PlayerSettings.cs
@@ -6,5 +6,16 @@ using System.Collections;
 
 public class PlayerSettings : MonoBehaviour
 {
+    [Header("Default player settings")]
+    private float defaultMovementSpeed = 5f;
+    private float defaultDashSpeed = 1f;
+    private float defaultDashDuation = 1f;
+    
+    private int defaultHealth = 100;
 
+    // References
+    public float DefaultMovementSpeed => defaultMovementSpeed;
+    public float DefaultDashSpeed => defaultDashSpeed;
+    public float DefaultDashDuration => defaultDashDuation;
+    public float DefaultHealth => defaultHealth;
 }
